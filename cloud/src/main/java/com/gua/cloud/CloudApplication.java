e12e943b072805551a9bcea87c59fcf93ba1f687
xionghao <18829037917@163.com>
2020-09-01 22:56:51 +0800

    第一次提交

---
+++ cloud/src/main/java/com/gua/cloud/CloudApplication.java
@@ -0,0 +1,16 @@
+package com.gua.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApplication.class, args);
+	}
+
+}
